ee97157c50d67833a46f6103f4ca7ff5beee7391
twoConstant <139519988+twoConstant@users.noreply.github.com>
2024-03-06 21:26:02 +0900

    Title: 수열 조작, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240306/수열 조작/sequence-manipulation.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        int n = sc.nextInt();
+
+        for(int i = 1; i < n + 1;i++) {
+            deque.addLast(i);
+        }
+
+        for(int i = 0; i < deque.size() - 1; i++){
+            deque.pollFirst();
+            int num = deque.pollFirst();
+            deque.addLast(num);
+        }
+        System.out.println(deque.peekLast());
+
+    }
+}
