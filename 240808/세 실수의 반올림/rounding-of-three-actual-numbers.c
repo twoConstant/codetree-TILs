7620821062c0cb3ee5fdbb0a73518ea79c9adc3d
twoConstant <139519988+twoConstant@users.noreply.github.com>
2024-08-08 12:35:04 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240808/세 실수의 반올림/rounding-of-three-actual-numbers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double num1_input, num2_input, num3_input;
+    scanf("%lf %lf %lf", &num1_input, &num2_input, &num3_input);
+    printf("%.3lf\n%.3lf\n%.3lf", num1_input, num2_input, num3_input);
+
+    return 0;
+}
