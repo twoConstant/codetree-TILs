2bd17b7e90f0213903927a9e300eeb7438a966b1
twoConstant <139519988+twoConstant@users.noreply.github.com>
2024-08-08 12:17:45 +0900

    Title: 입력받아 계산 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240808/입력받아 계산 2/input-calculate-2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int num1_input, num2_input;
+    scanf("%d %d", &num1_input, &num2_input);
+    printf("%d", num1_input * num2_input);
+    return 0;
+}
