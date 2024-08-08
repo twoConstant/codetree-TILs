d9cd8d1f01d892f5fa98002f351e4a1ffbd383a1
twoConstant <139519988+twoConstant@users.noreply.github.com>
2024-08-08 12:57:54 +0900

    Title: 문자열 순서 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240808/문자열 순서 바꾸기/change-order-of-strings.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char str1_input[100], str2_input[100];
+    scanf("%s %s", str1_input, str2_input);
+    printf("%s\n%s", str2_input, str1_input);
+
+    return 0;
+}
