1caddded6abe77e64ba70d245c055c808bb82dc9
twoConstant <139519988+twoConstant@users.noreply.github.com>
2024-08-08 12:28:57 +0900

    Title: 실수 입력받아 계산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240808/실수 입력받아 계산/calculation-by-inputting-a-real-number.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double num1_input, num2_input;
+    scanf("%lf %lf", &num1_input, &num2_input);
+    printf("%.2lf", num1_input + num2_input);
+    return 0;
+}
