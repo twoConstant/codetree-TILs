5c16e34374d0a1c88b0f77db269cdbdc3c55e126
twoConstant <139519988+twoConstant@users.noreply.github.com>
2024-08-08 12:39:02 +0900

    Title: 정수 세 개 입력받아 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240808/정수 세 개 입력받아 출력/take-three-integers-and-output.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int num1_input, num2_input, num3_input;
+    scanf("%d %d %d", &num1_input, &num2_input, &num3_input);
+    printf("%d %d %d", num1_input, num2_input, num3_input);
+    return 0;
+}
