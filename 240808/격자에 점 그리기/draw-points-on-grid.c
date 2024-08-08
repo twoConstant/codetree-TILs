090a98c630eb5c57f6dd3bee92278165a727243e
twoConstant <139519988+twoConstant@users.noreply.github.com>
2024-08-08 17:25:10 +0900

    Title: 격자에 점 그리기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240808/격자에 점 그리기/draw-points-on-grid.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int map_num[9][9] = {0,};
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int N, M;
+        scanf("%d%d", &N, &M);
+    
+
+    for(int t = 0; t < M; t++){
+        int i, j;
+            scanf("%d%d", &i, &j);
+        map_num[i - 1][j - 1] = t + 1;
+    }
+
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++) {
+            printf("%d ", map_num[i][j]);
+        }
+        printf("\n");
+    }
+
+    
+    return 0;
+}
